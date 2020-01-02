806fe5e922f3ffed82503004633cf3323e0222e6
kimzkf <842594235@qq.com>
2020-01-02 10:11:45 +0800

    test

---
+++ eu_client/src/main/java/com/example/bean/Order.java
@@ -0,0 +1,33 @@
+package com.example.bean;
+
+import java.util.Date;
+
+public class Order {
+        private String id;
+        private String name;
+        private Date date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}

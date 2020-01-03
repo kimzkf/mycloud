5dc1739c35d2bcc719db459ced1638794a9e7c25
kimzkf <842594235@qq.com>
2020-01-03 11:23:22 +0800

    使用consul替代eureka

---
+++ consul1/src/test/java/com/example/consul1/Consul1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consul1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consul1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

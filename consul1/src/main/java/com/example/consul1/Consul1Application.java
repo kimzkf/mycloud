f8393a86be583c87c7a8a22b97f9c61f02a43ffb
kimzkf <842594235@qq.com>
2020-01-05 20:25:55 +0800

    swagger集成在网关中

--- consul1/src/main/java/com/example/consul1/Consul1Application.java
+++ consul1/src/main/java/com/example/consul1/Consul1Application.java
@@ -1,6 +1,7 @@
 package com.example.consul1;
 
-import feign.Retryer;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,12 +11,14 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient//启用服务注册与发现
 @EnableFeignClients//开启feign客户端
 @EnableHystrix//开启熔断，下面的frignRetryer就不需要了
 @EnableHystrixDashboard//可视化面板http://127.0.0.1:8504/hystrix
+@EnableSwagger2Doc //开启swagger生成文档
 public class Consul1Application {
     @Bean //相当
     // 于xml中的bean标签，主要是用于调用当前方法获取指定对象
--- mygetway1/src/main/java/com/getway/mygetway1/Mygetway1Application.java
+++ mygetway1/src/main/java/com/getway/mygetway1/Mygetway1Application.java
@@ -2,15 +2,24 @@ package com.getway.mygetway1;
 
 import com.getway.mygetway1.authFilter.RequestFilter;
 import com.getway.mygetway1.authFilter.TimeFilter;
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient//启用服务注册与发现
+@EnableSwagger2Doc//开启swagger
 public class Mygetway1Application {
 
     public static void main(String[] args) {
@@ -27,4 +36,24 @@ public class Mygetway1Application {
                 //.route(r->r.path("/api-member/**").filters(f->f.stripPrefix(1)).uri("lb://consul3"))
                 .build();
     }
+    //添加文档来源
+    @Component
+    @Primary
+    class DocumentationConfig implements SwaggerResourcesProvider{
+        @overide
+        public List<SwaggerResource> get(){
+            List resources=new ArrayList();
+            resources.add(swaggerResource("会员服务"，"/api-member/v2/api-docs","0.1"));
+            resources.add(swaggerResource("订单服务"，"/api-order/v2/api-docs","0.1"));
+            return resources;
+        }
+        private SwaggerResource swaggerResource(Strign name,String location,String version){
+            SwaggerResource swaggerResource=new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
+    }
+
 }
